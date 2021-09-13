aab4bb33b707730deb19c6f1a03cd77dc77afd8e
flopmingo <60086882+flopmingo@users.noreply.github.com>
2021-09-12 22:52:29 -0700

    Create search_ui.py

---
+++ A3-search_engine/src/search_ui.py
@@ -0,0 +1,21 @@
+from flask import Flask,request,render_template
+from IndexSearchM4 import IndexSearch
+app=Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('webui.html')
+
+@app.route('/',methods=['POST'])
+def my_form_post():
+    text=request.form['u']
+    obj=IndexSearch()
+    urls = obj.searchIndex(text)
+    
+    url_str = ''
+    for i in urls:
+        url_str += str(i) + '<br/>'
+    return url_str
+
+if __name__ =="__main__":
+    app.run()
